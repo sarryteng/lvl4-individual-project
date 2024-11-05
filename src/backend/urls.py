d2814832f3723ce93e498366e00513badfcad16d
“Sarry <2721338t@student.gla.ac.uk>
2024-11-05 23:52:02 +0000

    subfolder in learn + base templates for learn section

--- src/backend/urls.py
+++ src/backend/urls.py
@@ -1,7 +1,16 @@
-from .views import home, learn, guides, tools
+from .views import *
 
 def register_routes(app):
     app.add_url_rule('/', 'home', home)
     app.add_url_rule('/learn', 'learn', learn)
+    app.add_url_rule('/learn/mobile_security', 'mobile_security', mobile_security)
+    app.add_url_rule('/learn/encryption', 'encryption', encryption)
+    app.add_url_rule('/learn/strong_passwords', 'strong_passwords', strong_passwords)
+    app.add_url_rule('/learn/two_factor_authentication', 'two_factor_authentication', two_factor_authentication)
+    app.add_url_rule('/learn/phishing', 'phishing', phishing)
+    app.add_url_rule('/learn/software_updates', 'software_updates', software_updates)
+    app.add_url_rule('/learn/identity_theft', 'identity_theft', identity_theft)
+    app.add_url_rule('/learn/malware', 'malware', malware)
+    app.add_url_rule('/learn/vpn', 'vpn', vpn)
     app.add_url_rule('/guides', 'guides', guides)
     app.add_url_rule('/tools', 'tools', tools)
--- src/backend/views.py
+++ src/backend/views.py
@@ -3,11 +3,43 @@ from flask import render_template
 def home():
     return render_template('home.html')
 
+# ------------------- LEARN SECTION: ------------------- 
 def learn():
     return render_template('learn.html')
 
+def mobile_security():
+    return render_template('learn/mobile_security.html')
+
+def encryption():
+    return render_template('learn/encryption.html')
+
+def strong_passwords():
+    return render_template('learn/strong_passwords.html')
+
+def two_factor_authentication():
+    return render_template('learn/two_factor_authentication.html')
+
+def phishing():
+    return render_template('learn/phishing.html')
+
+def software_updates():
+    return render_template('learn/software_updates.html')
+
+def identity_theft():
+    return render_template('learn/identity_theft.html')
+
+def malware():
+    return render_template('learn/malware.html')
+
+def vpn():
+    return render_template('learn/vpn.html')
+
+
+# ------------------- GUIDES SECTION: ------------------- 
 def guides():
     return render_template('guides.html')
 
+
+# ------------------- TOOLS SECTION: ------------------- 
 def tools():
     return render_template('tools.html')
