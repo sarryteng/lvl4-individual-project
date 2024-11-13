33459caeef9541b2c1002b8ad81373d2a45daf47
“Sarry <2721338t@student.gla.ac.uk>
2024-11-13 23:57:14 +0000

    base pages for "guide"

--- src/backend/views.py
+++ src/backend/views.py
@@ -42,6 +42,30 @@ def guides():
 def secure_mobile():
     return render_template('guides/secure-mobile.html')
 
+def use_encryption():
+    return render_template('guides/use-encryption.html')
+
+def secure_passwords():
+    return render_template('guides/secure-passwords.html')
+
+def use_two_factor():
+    return render_template('guides/use-two-factor.html')
+
+def spot_phishing():
+    return render_template('guides/spot-phishing.html')
+
+def how_to_update_software():
+    return render_template('guides/how-to-update-software.html')
+
+def prevent_theft():
+    return render_template('guides/prevent-theft.html')
+
+def malware_protection():
+    return render_template('guides/malware-protection.html')
+
+def use_vpn():
+    return render_template('guides/use-vpn.html')
+
 
 # ------------------- TOOLS SECTION: ------------------- 
 def tools():
